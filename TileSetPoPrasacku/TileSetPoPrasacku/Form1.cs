7588aba0567bb24daf4fd4328aaa31de7be656ea
gucisbucis <50733238+gucisbucis@users.noreply.github.com>
2021-04-29 22:31:29 +0200

    Add files via upload

---
+++ TileSetPoPrasacku/TileSetPoPrasacku/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TileSetPoPrasacku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[,] tileNames =
+            {
+                 {"rock","sand", "grass",  "sand","sand","grass","sand","sand","grass","rock"}
+                ,{"sand","grass", "sand",  "sand","rock","sand","sand","sand","sand","sand"}
+                ,{"sand","grass", "sand",  "sand","sand","sand","sand","sand","sand","sand"}
+                ,{"sand","grass", "sand",  "sand","sand","sand","sand","grass","sand","sand"}
+                ,{"sand","rock", "water",  "sand","grass","rock","sand","sand","rock","sand"}
+                ,{"sand","sand", "sand",  "sand","sand","sand","grass","sand","sand","sand"}
+                ,{"sand","grass", "rock",  "sand","grass","sand","sand","sand","sand","sand"}
+                ,{"sand","sand", "sand",  "sand","sand","sand","sand","sand","sand","sand"}
+                ,{"rock","sand", "sand",  "sand","sand","rock","grass","sand","sand","sand"}
+                ,{"sand","grass", "sand",  "sand","sand","rock","sand","sand","rock","grass"}
+            };
+
+            mapa = new Mapa(tileNames);
+            this.trackBar1.Maximum = tileNames.GetLength(0) - tilesOnScreenX;
+            this.trackBar2.Maximum = tileNames.GetLength(1) - tilesOnScreenY;
+            DrawMap();
+        }
+
+        private void DrawMap()
+        {
+            this.pictureBox1.Image = mapa.GetMap(trackBar1.Value, trackBar2.Value, tilesOnScreenX, tilesOnScreenY);
+        }
+
+        int tilesOnScreenX = 5;
+        int tilesOnScreenY = 5;
+        Mapa mapa;
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            DrawMap();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            DrawMap();
+        }
+    }
+}
---
+++ TileSetPoPrasacku/TileSetPoPrasacku/Mapa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileSetPoPrasacku
+{
+    class Mapa : IDisposable
+    {
+        public Mapa(string[,] tiles)
+        {
+            int columns = tiles.GetLength(0);
+            int rows = tiles.GetLength(1);
+            map = new System.Drawing.Bitmap[columns,rows];
+            for(int x=0; x<columns; ++x)
+            {
+                for(int y=0; y<rows; ++y)
+                {
+                    string path = $"Tiles\\{tiles[x, y]}.png";
+                    Bitmap bmp = new Bitmap(path);
+                    map[x, y] = bmp;
+                }
+            }
+        }
+        public Bitmap GetTile(int x, int y)
+        {
+            return map[x, y];
+        }
+
+        public int tileWidth => GetTile(0, 0).Width;
+        public int tileHeight => GetTile(0, 0).Height;
+
+        public Bitmap GetMap(int x, int y, int xNum, int yNum)
+        {
+            Bitmap ret = new Bitmap(tileWidth * xNum, tileHeight * yNum);
+            Graphics g = Graphics.FromImage(ret);
+            for(int i=0; i<xNum; ++i)
+            {
+                for(int j=0; j<yNum; ++j)
+                {
+                    int posY = i * tileWidth;
+                    int posX = j * tileHeight;
+                    g.DrawImage(GetTile(x+i, y+j), posX, posY);
+                }
+            }
+            return ret;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private Bitmap[,] map;
+    }
+}
